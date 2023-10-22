2f04519d859ffea21f5b72796fd7e17596421efc
Bartas Dausynas <bartas3@gmail.com>
2023-10-22 17:39:18 +0300

    button fixed

--- index.js
+++ index.js
@@ -3,15 +3,12 @@ const btn1 = document.getElementById('btn1');
 const btn2 = document.getElementById('btn2');
 
 btn1.addEventListener('click', function onClick() {
-  btn1.style.backgroundColor = 'mediumslateblue';
-  btn1.style.color = 'white';
-  btn2.style.backgroundColor = 'white';
-  btn2.style.color = 'mediumslateblue';
+    btn1.classList.add('active');
+    btn2.classList.remove('active');
 });
 
 btn2.addEventListener('click', function onClick() {
-  btn2.style.backgroundColor = 'mediumslateblue';
-  btn2.style.color = 'white';
-  btn1.style.backgroundColor = 'white';
-  btn1.style.color = 'mediumslateblue';
+    btn2.classList.add('active');
+    btn1.classList.remove('active');
 });
+
